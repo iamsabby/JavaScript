b76d73a84049bfd0397e3747a46b4bd0dad62c68
iamsabby <sohailsabby@gmail.com>
2024-07-04 21:03:36 +0500

    13th Commit

---
+++ 14-Exercise 5 Hackerman/script.js
@@ -0,0 +1,20 @@
+setTimeout(() => {
+    console.log("fetching username....")
+    // document.write("fetching username....")
+  }, 2000)
+  setTimeout(() => {
+    console.log("Username fetched")
+  }, 4000)
+  setTimeout(() => {
+    console.log("@i_am.sabby")
+  }, 5500)
+  setTimeout(() => {
+    console.log("fetching password....")
+  }, 6000)
+  setTimeout(() => {
+    console.log("connecting to instagram....")
+  }, 7000)
+  setTimeout(() => {
+    window.open("https://www.instagram.com/i_am.sabby/", "_blank");
+  }, 7500)
+  
